d107c57ba20f36e25428b8b8e3de7ebec9b309fd
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2024-02-15 19:32:29 -0300

    Add files via upload

---
+++ assets/js/program.js
@@ -0,0 +1,32 @@
+/* !TROCA DE ICONES! */
+/* !WhatsApp! */
+function mouseEmCimaWhatsApp(imagem) {
+    imagem.src='assets/img/icons/whatsapp_colorido.png'
+}
+ function mouseForaWhatsApp(imagem) {
+    imagem.src='assets/img/icons/whatsapp.png'
+}
+
+/* !Linkedin! */
+function mouseEmCimaLinkedin(imagem) {
+    imagem.src='assets/img/icons/linkedin_colorido.png'
+}
+ function mouseForaLinkedin(imagem) {
+    imagem.src='assets/img/icons/linkedin.png'
+}
+
+/* !Email! */
+function mouseEmCimaEmail(imagem) {
+    imagem.src='assets/img/icons/email_colorido.png'
+}
+ function mouseForaEmail(imagem) {
+    imagem.src='assets/img/icons/o-email.png'
+}
+
+/* !Instagram! */
+function mouseEmCimaInstagram(imagem) {
+    imagem.src='assets/img/icons/instagram_colorido.png'
+}
+ function mouseForaInstagram(imagem) {
+    imagem.src='assets/img/icons/instagram.png'
+}
